06073979b369d878c56962b39d6219bd42534909
MacUser <macuser@MacUser-no-MacBook-Air.local>
2015-12-08 17:13:04 +0900

    敵キャラ(攻撃で倒せるまで)

---
+++ Enemy1Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Script : MonoBehaviour {
+
+	Rigidbody2D rigidbody2D;
+	public int speed = -3;
+	
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
+	}
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag == "Slash") {
+			Destroy(gameObject);
+		}
+	}
+}
